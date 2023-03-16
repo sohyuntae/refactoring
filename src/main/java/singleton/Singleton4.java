25e9aa92dd45051d32af3908ffac9494cececbcb
hyuntae <sotae69@naver.com>
2023-03-16 14:53:39 +0900

    싱글턴 관련 예제 내용 추가 보강.

--- src/main/java/singleton/Singleton4.java
+++ src/main/java/singleton/Singleton4.java
@@ -14,6 +14,7 @@ public class Singleton4 {
     /* Java 진영에서 가장 많이 사용되는 LazyHolder 를 사용하는 싱글톤 패턴
     * private static class 인 LazyHolder 안에 instace 를 final 로 선언하는 방법
     * JVM (Java Virtual Machine) 의 클래스의 초기화 과정에서 원자성을 보장하는 원리를 이용하는 방식 이다.
+    * LazyHolder 는 JVM 자체의 특성을 최대한 이끌어내어 성능저하를 막는 나름대로 우아한 방식이라고 할 수 있다.
     * */
     public static Singleton4 getInstance() {
         return LazyHolder.instance;
