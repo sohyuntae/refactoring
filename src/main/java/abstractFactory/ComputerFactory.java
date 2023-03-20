53e99777e212870302dcc74348d83d87e521610a
hyuntae <sotae69@naver.com>
2023-03-20 17:36:44 +0900

    추상 팩토리 관련 예제.

---
+++ src/main/java/abstractFactory/ComputerFactory.java
@@ -0,0 +1,7 @@
+package abstractFactory;
+
+public class ComputerFactory {
+    public static Computer getComputer(ComputerAbstractFactory factory){
+        return factory.createComputer();
+    }
+}
---
+++ src/main/java/abstractFactory/ServerFactory.java
@@ -0,0 +1,18 @@
+package abstractFactory;
+
+public class ServerFactory implements ComputerAbstractFactory {
+    private String ram;
+    private String hdd;
+    private String cpu;
+
+    public ServerFactory(String ram, String hdd, String cpu) {
+        this.ram = ram;
+        this.hdd = hdd;
+        this.cpu = cpu;
+    }
+
+    @Override
+    public Computer createComputer() {
+        return new PC(ram, hdd,cpu);
+    }
+}
