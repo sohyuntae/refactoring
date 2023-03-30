2682d0cd9749da3716de8bdf363668cc5a9b03e1
hyuntae <sotae69@naver.com>
2023-03-30 16:47:18 +0900

    어댑터 패턴 예제 정리.

---
+++ src/main/java/structuralPattern/adapter/AdapterMain.java
@@ -0,0 +1,45 @@
+package structuralPattern.adapter;
+
+/* 어댑터 패턴을 구현하기 위해서는 Class Adapter와 Object Adapter 두 가지 방법이 있다.
+ *  - Class Adapter - 자바의 상속(Inheritance)을 이용한 방법
+ *  - Object Adapter - 자바의 합성(Composite)을 이용한 방법
+ * */
+public class AdapterMain {
+    public static void main(String[] arg) {
+        testClassAdapter();
+        testObjectAdapter();
+    }
+
+    private static void testObjectAdapter() {
+        SocketAdapter sockAdapter = new SocketObjectAdapterImpl();
+        Volt v3 = getVolt(sockAdapter, 3);
+        Volt v12 = getVolt(sockAdapter, 12);
+        Volt v120 = getVolt(sockAdapter, 120);
+        System.out.println("v3 volts using Object Adapter=" + v3.getVolts());
+        System.out.println("v12 volts using Object Adapter=" + v12.getVolts());
+        System.out.println("v120 volts using Object Adapter=" + v120.getVolts());
+    }
+
+    private static void testClassAdapter() {
+        SocketAdapter sockAdapter = new SocketClassAdapterImpl();
+        Volt v3 = getVolt(sockAdapter, 3);
+        Volt v12 = getVolt(sockAdapter, 12);
+        Volt v120 = getVolt(sockAdapter, 120);
+        System.out.println("v3 volts using Class Adapter=" + v3.getVolts());
+        System.out.println("v12 volts using Class Adapter=" + v12.getVolts());
+        System.out.println("v120 volts using Class Adapter=" + v120.getVolts());
+    }
+
+    private static Volt getVolt(SocketAdapter sockAdapter, int i) {
+        switch (i) {
+            case 3:
+                return sockAdapter.get3Volt();
+            case 12:
+                return sockAdapter.get12Volt();
+            case 120:
+                return sockAdapter.get120Volt();
+            default:
+                return sockAdapter.get120Volt();
+        }
+    }
+}
---
+++ src/main/java/structuralPattern/adapter/Socket.java
@@ -0,0 +1,7 @@
+package structuralPattern.adapter;
+
+public class Socket {
+    public Volt getVolt() {
+        return new Volt(120);
+    }
+}
